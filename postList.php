1569f365b6b46165d7c35c9541edba339f72ebf2
Haebaru Kana <kana.sakura1106@gmail.com>
2019-01-22 07:29:13 +0900

    Fixed file

--- postList.php
+++ postList.php
@@ -83,11 +83,11 @@ if(!empty($dbPostList)){
 			<!-- $login_flgをscript.jsに渡すための記述 -->
 			<?php $login_flg = !empty($_SESSION['user_id']); ?>
 			<script>var login_flg = "<?php echo $login_flg ?>"</script>
-			<div class="btn-good <?php if(isGood($_SESSION['user_id'], $dbPostList[$key]['id'])) echo 'active'; ?>">
+			<div class="btn-good <?php if(isGood(isset($_SESSION['user_id']), $dbPostList[$key]['id'])) echo 'active'; ?>">
 				<!-- 自分がいいねした投稿にはハートのスタイルを常に保持する -->
 				<i class="fa-heart fa-lg px-16
 				<?php
-					if(isGood($_SESSION['user_id'],$dbPostList[$key]['id'])){
+					if(isGood(isset($_SESSION['user_id']),$dbPostList[$key]['id'])){
 						echo ' active fas';
 					}else{
 						echo ' far';
